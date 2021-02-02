bcb8887fc9d0d206a4ef2fbedc8ab1c69f6f8448
jungdolee <jungdo1.lee@gmail.com>
2021-02-02 17:13:23 +0900

    minor up

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello V-Pay</h1>
-      <h2>React and TypeScript starter project</h2>
+      <h2>2021 Coming Soon</h2>
       <img src="https://www.hanwhalife.com.vn/public/uploads/UL%202020/UL-KV-1600x650.jpg" ></img>
     </div>
   );
