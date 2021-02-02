09ee4b2355fe0687ae634e0293280dc669f321b8
jungdolee <jungdo1.lee@gmail.com>
2021-02-02 11:39:53 +0900

    img update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
 
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello V-Pay</h1>
       <h2>React and TypeScript starter project</h2>
+      <img src="https://www.hanwhalife.com.vn/public/uploads/UL%202020/UL-KV-1600x650.jpg" ></img>
     </div>
   );
 }
